2df2f0eb241ecf7da1d3d73e831df5d817a8bcd7
Ingrid Kamga <xingridkamga@gmail.com>
2023-12-09 21:34:08 +0100

    My solution to Level 1

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,7 +1,7 @@
 '''
 ////////////////////////////////////////////////////////////
 ///                                                      ///
-///   0. tests.py is passing but the code is vulnerable  /// 
+///   0. tests.py is passing but the code is vulnerable  ///
 ///   1. Review the code. Can you spot the bug?          ///
 ///   2. Fix the code but ensure that tests.py passes    ///
 ///   3. Run hack.py and if passing then CONGRATS!       ///
@@ -12,22 +12,26 @@
 '''
 
 from collections import namedtuple
+from math import fabs
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
-    net = 0
-    
+    payment_net = 0
+    product_net = 0
+
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            payment_net += item.amount
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            product_net += item.amount * item.quantity
         else:
             return("Invalid item type: %s" % item.type)
-    
-    if net != 0:
+
+    net = payment_net - product_net
+
+    if fabs(net) > 1e-5:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
         return("Order ID: %s - Full payment received!" % order.id)
