41069c6c2bd1f43bd7bf9ec82a8409d852495be4
Ingrid Kamga <xingridkamga@gmail.com>
2023-12-10 08:11:20 +0100

    Remove references to SHA256 for password hashing

--- Level-5/code.py
+++ Level-5/code.py
@@ -18,32 +18,22 @@ class Random_generator:
     def generate_salt(self, rounds=12):
         return bcrypt.gensalt(rounds)
 
-class SHA256_hasher:
+class bcrypt_hasher:
 
     # produces the password hash by combining password + salt because hashing
     def password_hash(self, password, salt):
-        password = binascii.hexlify(hashlib.sha256(password.encode()).digest())
+        password = binascii.hexlify(password.encode())
         password_hash = bcrypt.hashpw(password, salt)
         return password_hash.decode('ascii')
 
     # verifies that the hashed password reverses to the plain text version on verification
     def password_verification(self, password, password_hash):
-        password = binascii.hexlify(hashlib.sha256(password.encode()).digest())
+        password = binascii.hexlify(password.encode())
         password_hash = password_hash.encode('ascii')
         return bcrypt.checkpw(password, password_hash)
-
-class MD5_hasher:
-
-    # same as above but using a different algorithm to hash which is MD5
-    def password_hash(self, password):
-        return hashlib.md5(password.encode()).hexdigest()
-
-    def password_verification(self, password, password_hash):
-        password = self.password_hash(password)
-        return secrets.compare_digest(password.encode(), password_hash.encode())
 
 # a collection of sensitive secrets necessary for the software to operate
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
 PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY')
-PASSWORD_HASHER = 'SHA256_hasher'
+PASSWORD_HASHER = 'bcrypt_hasher'
